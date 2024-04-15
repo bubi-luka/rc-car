49607f835b9e9eaa887c51e2dbf0284a48d20b6b
Luka Oman <bubi-luka@users.noreply.github.com>
2024-04-15 19:04:15 +0200

    FEATURE: receive data from the RX
    1. Change protocol for communication between RX and Arduino from iBUS
    	to PPM.
    2. Receive data from RX.
    3. Parse data from RX.
    4. Change README.md file with those changes, credit the library and
    	author.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 - wires
 
 ### Used libraries
-- Library for iBus communication: [iBusBM](https://github.com/bmellink/IBusBM/)
+- Library for PPM communication: [Arduino-PPM](https://github.com/Lynxmotion/Arduino-PPM)
 
 **!! Thank you all for your work !!**
 
@@ -37,6 +37,21 @@
 ### 1.0.0
 - code cleanup
 
+### 0.4.0
+- code optimization
+
+### 0.3.0
+- calculate motor power from receiver data
+
 ## Change Log
+### 0.2.0
+- get data from the receiver
+- parse data into human readable from
+
+### 0.1.1
+- change planned iBus protocol with PPM - more suitable for our receiver, since we
+  can connect our receiver on any port of the Arduino (exept TX and RX ports) and
+  not block sketch upload.
+
 ### 0.1.0
 - initial setup
--- rc-car.ino
+++ rc-car.ino
@@ -1,26 +1,79 @@
 /* *************************************************************************** *
- * Project Title: 
+ * Project Title: RC car
  * 
- * Description: 
+ * Description: A small Arduino project that brings old and broken Nikko
+ * VaporizR 2 car back to life. We use new motors, new motor driver board, 
+ * an iA6C receiver module and a power step-down module to make use of the 
+ * structure of the car useful again.
  *
- * Licence: GPL 3.0
- * Version: 0.0.0
- * created: DD. MM. YYYY
- * by:      FirstName LastName
+ * Licence: GPL 3.0       (details in project file "LICENCE")
+ * Version: 0.1.0         (details in project file "README.md")
+ * created: 14. 04. 2024
+ * by:      Luka Oman
  * *************************************************************************** */
 
 // Declare additional libraries
+#include <ppm.h>
+
 // Define Arduino pins
+static const int receiverPin = 3;
+
 // Define new objects
+
+// Define constants
+#define channelSafeMode 6        // kill switch
+#define channelLeftRight 1       // turn left or right
+#define channelForwardReverse 2  // go forward or reverse
+
 // Define variables
+bool carOn = false;         // kill switch
+bool motorAForward = true;  // determinate motor A direction (not to burn the motor driver)
+bool motorBForward = true;  // determinate motor B direction (not to burn the motor driver)
+
 // Define custom functions
 
 // Initialization function, run only once
 void setup() {
-
+  Serial.begin(9600);
+  ppm.begin(receiverPin, false);
 }
 
 // Main function, run in a loop
 void loop() {
 
+  // get channels values
+  short getSafeMode = ppm.read_channel(channelSafeMode);
+  short getLeftRight = ppm.read_channel(channelLeftRight);
+  short getForwardReverse = ppm.read_channel(channelForwardReverse);
+
+  if (getSafeMode < 1600) {
+    carOn = false;
+  } else {
+    carOn = true;
+  }
+
+  if (carOn == true) 
+    Serial.print("Car is ON\t|");
+  } else {
+    Serial.print("Car is OFF\t|");
+  }
+
+  if (getLeftRight < 1400) {
+    Serial.print(" Left\t|");
+  } else if (getLeftRight > 1600) {
+    Serial.print(" Right\t|");
+  } else {
+    Serial.print(" Center\t|");
+  }
+
+  if (getForwardReverse < 1400) {
+    Serial.print(" Reverse\t|");
+  } else if (getForwardReverse > 1600) {
+    Serial.print(" Forward\t|");
+  } else {
+    Serial.print(" Stop\t|");
+  }
+
+  Serial.println();
+  delay(50);
 }
