68c7b89aeb04c804158a9c1eae0aff7ca798143e
Luka Oman <bubi-luka@users.noreply.github.com>
2024-04-16 09:58:05 +0200

    FEATURE: parsing the throttle input
    With parsing the throttle input we get the percentage of throttle and
    the direction of the RC car the RC driver wants.
    We also protect the motor driver from overloading by detecting the
    direction state change and apply delay (will be changed later).

--- rc-car.ino
+++ rc-car.ino
@@ -25,10 +25,22 @@ static const int receiverPin = 3;
 #define channelLeftRight 1       // turn left or right
 #define channelForwardReverse 2  // go forward or reverse
 
+// in theory the signal can go from 1000 (low) to 1500 (neutral) and from 1500 (neutral) to 2000 (high)
+// but in reallity those values can vary so we use user defined constants
+static const int lowNegative = 980;    // lowest signal received
+static const int highNegative = 1450;  // low breaking point for neutral signal
+static const int lowPositive = 1550;   // high breaking point for neutral signal
+static const int highPositive = 2020;  // highest signal received
+
 // Define variables
-bool carOn = false;         // kill switch
+bool carOn = false;  // kill switch
+/* We do not need this part of code (yet)
 bool motorAForward = true;  // determinate motor A direction (not to burn the motor driver)
 bool motorBForward = true;  // determinate motor B direction (not to burn the motor driver)
+*/
+
+int throttleIndex = 0;    // 0% => stop, 100% => full power
+int motorDirection = 0;  // -1 => reverse, 0 => stop, 1 => forward
 
 // Define custom functions
 
@@ -46,34 +58,90 @@ void loop() {
   short getLeftRight = ppm.read_channel(channelLeftRight);
   short getForwardReverse = ppm.read_channel(channelForwardReverse);
 
-  if (getSafeMode < 1600) {
-    carOn = false;
-  } else {
-    carOn = true;
-  }
+  // determinate limits, so the misconfigured receiver can not break code
+  carOn = (getSafeMode < lowPositive) ? false : true;
 
-  if (carOn == true) 
+  getLeftRight = (getLeftRight < lowNegative) ? lowNegative : getLeftRight;
+  getLeftRight = (getLeftRight > highPositive) ? highPositive : getLeftRight;
+
+  getForwardReverse = (getForwardReverse < lowNegative) ? lowNegative : getForwardReverse;
+  getForwardReverse = (getForwardReverse > highPositive) ? highPositive : getForwardReverse;
+
+  // Write current state of the received values => simple parsing
+  if (carOn == true) {
     Serial.print("Car is ON\t|");
   } else {
     Serial.print("Car is OFF\t|");
   }
 
-  if (getLeftRight < 1400) {
-    Serial.print(" Left\t|");
-  } else if (getLeftRight > 1600) {
-    Serial.print(" Right\t|");
+  if (getLeftRight < highNegative) {
+    Serial.print(" Left   |");
+  } else if (getLeftRight > lowPositive) {
+    Serial.print(" Right  |");
   } else {
-    Serial.print(" Center\t|");
+    Serial.print(" Center |");
   }
 
-  if (getForwardReverse < 1400) {
-    Serial.print(" Reverse\t|");
-  } else if (getForwardReverse > 1600) {
-    Serial.print(" Forward\t|");
+  /* ***** D E P R E C A T E D - C O D E *****
+  if (getForwardReverse < highNegative) {
+    Serial.print(" Reverse |");
+  } else if (getForwardReverse > lowPositive) {
+    Serial.print(" Forward |");
   } else {
-    Serial.print(" Stop\t|");
+    Serial.print(" Stop    |");
   }
+*/
+
+  // real parsing of the code, used to move the car in the desired direction
+  if (carOn == true) {
+
+    // get the throttle index, that is the persentage of throttle the driver behing the TX want
+    if ((motorDirection < 0 && getForwardReverse >= highNegative) || (motorDirection > 0 && getForwardReverse <= lowPositive)) {
+      // STOP => we are changing direction and would not want to overload the motor driver
+      motorDirection = 5;  // <= has to be "0", we are using other number just to debug this state
+      throttleIndex = 0;
+      delay(1000);  // <= has to be "500" milliseconds, we are using higher delay just to debug this state
+    } else if (getForwardReverse < lowPositive && getForwardReverse > highNegative) {
+      // STOP => the TX stick is in neutral position
+      motorDirection = 0;
+      throttleIndex = 0;
+    } else if (getForwardReverse >= lowPositive) {
+      // FORWARD
+      throttleIndex = map(getForwardReverse, lowPositive, highPositive, 0, 100);
+      motorDirection = 1;
+    } else if (getForwardReverse <= highNegative) {
+      // REVERSED
+      throttleIndex = map(getForwardReverse, highNegative, lowNegative, 0, 100);
+      motorDirection = -1;
+    }
+
+    (motorDirection >= 0) ? Serial.print("  ") : Serial.print(" ");
+    Serial.print(motorDirection);
+    Serial.print(" | ");
+    Serial.print(throttleIndex);
+    Serial.print(" | ");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   Serial.println();
-  delay(50);
+  delay(10);
 }
